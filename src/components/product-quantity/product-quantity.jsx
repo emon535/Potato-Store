07c6481852cf352d845a14f28c2f7366dc872f00
emon535@gmail.com <emon535@gmail.com>
2019-12-08 21:07:40 +0600

    add button fixed

--- src/components/product-quantity/product-quantity.jsx
+++ src/components/product-quantity/product-quantity.jsx
@@ -1,48 +1,50 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './product-quantity.scss';
-import {AddToCart, RemoveFromCart, getItemCount} from  '../../services/myCart';
+import { AddToCart, RemoveFromCart, getItemCount } from '../../services/myCart';
 
-export default function ProductQuantity({item}) {
+export default function ProductQuantity({ item }) {
   const [count, setCount] = useState();
   let x = 0;
-  useEffect(()=>{
-   
+  useEffect(() => {
+
     setCount(getItemCount(item.id));
     console.log(count, 'cia tas count qualitcio');
-    
-  },[count]);
+
+  }, [count]);
 
 
   function handleCount(symbol) {
-    if(symbol == '+') {
+    if (symbol == '+') {
       AddToCart(item);
-      setCount(count+1);
+      setCount(count + 1);
     }
-    if(symbol == '-') {
-     
-      if(count>0){
-        setCount(count-1);
+    if (symbol == '-') {
+
+      if (count > 0) {
+        setCount(count - 1);
         RemoveFromCart(item.id);
       }
-     
+
     }
   }
 
   return (
     <>
-    <div className="container">
+      <div className="container">
         <div className="container__button" >
           {/* <button onClick={()=>RemoveFromCart(item.id)}>-</button>  */}
-          <button onClick={()=>handleCount('-')}>-</button> 
-          </div>
-          <div className="container">
+          <button onClick={() => handleCount('-')}>-</button>
+        </div>
+        <div className="container">
+          <div className="container__count">
             {count}
           </div>
-          <div className="container__button">
-            <button onClick={()=>handleCount('+')} >+</button>
-            {/* <button onClick={()=>AddToCart(item)} >+</button> */}
         </div>
-    </div>
+        <div className="container__button">
+          <button onClick={() => handleCount('+')} >+</button>
+          {/* <button onClick={()=>AddToCart(item)} >+</button> */}
+        </div>
+      </div>
     </>
   );
 }
