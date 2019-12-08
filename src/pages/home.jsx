7bc02e387d90c48904fb9cc578ee10e3692d9827
emon535@gmail.com <emon535@gmail.com>
2019-12-08 20:37:58 +0600

    navbar fixed and start shopping homapage

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,24 +1,24 @@
 import React, { useState, useEffect } from 'react';
-import {getAllProducts} from '../services/products-service';
+import { getAllProducts } from '../services/products-service';
 import ProductCard from '../components/product-card/product-card';
 import ApiFactory from '../mock';
 import Editor from '../Editor';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './home.scss';
-export default function Home({itemss}) {
+export default function Home({ itemss }) {
 
-  const [items, setItems]= useState([]);
-  const [error, setError]= useState('');
+  const [items, setItems] = useState([]);
+  const [error, setError] = useState('');
 
 
   useEffect(() => {
     ApiFactory.getInstance().get('/api/products')
       .then((res) => {
         setItems(res.data);
-       // console.log(res.data, 'cia data');
+        // console.log(res.data, 'cia data');
       })
-      .catch((error) => { setError(error.res)}) 
-      
+      .catch((error) => { setError(error.res) })
+
   }, [error]);
 
 
@@ -28,27 +28,27 @@ export default function Home({itemss}) {
     <>
       <div className="welcome-image body">
         <div className="welcome-text">
-           <h1>Potato shop</h1>
-           <h3>Highly pleasant to taste</h3>
-          <Link to="/products" className="welcome-button">Start shopping</Link>
+          <h1>Potato shop</h1>
+          <h3>Highly pleasant to taste</h3>
+          <Link to="/products" className="welcome-button">START SHOPPING</Link>
         </div>
       </div>
-      { items.map((item, index) => {
-      return <div className="flex-item" key={index}>
-                <img className="flex-item__image" heigth="239"width="329" src={item.images[0].url} alt=""/>
-                <div className="flex-item__info">          
-                  <h1>{item.name}</h1>
-                  <p>{item.description}</p>                  
-                </div>
-              </div>
+      {items.map((item, index) => {
+        return <div className="flex-item" key={index}>
+          <img className="flex-item__image" heigth="239" width="329" src={item.images[0].url} alt="" />
+          <div className="flex-item__info">
+            <h1>{item.name}</h1>
+            <p>{item.description}</p>
+          </div>
+        </div>
       })}
-      
+
 
     </>
   );
 }
 
 Home.defaultProps = {
-  items:[]
+  items: []
 }
 
