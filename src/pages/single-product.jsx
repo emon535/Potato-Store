c34efdf19892b6c173007937e22c09a4b76531c8
emon535@gmail.com <emon535@gmail.com>
2019-12-10 21:14:20 +0600

    Form Design done for mobile and full screen

--- src/pages/single-product.jsx
+++ src/pages/single-product.jsx
@@ -1,18 +1,18 @@
 import ApiFactory from '../mock';
 import './single-product.scss';
 import React, { Component } from 'react'
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-let idGenerator =10;
+let idGenerator = 10;
 export default class Product extends Component {
-  
-    constructor(props){
+
+    constructor(props) {
         super(props);
         let query = this.props.match.params.productId;
-        this.state = {id:query, name: 'a', description:'aaa', price:0, images:[], newImage:'',new:false };
-        
+        this.state = { id: query, name: 'a', description: 'aaa', price: 0, images: [], newImage: '', new: false };
+
     }
-    
+
     async componentDidMount() {
         let id = this.props.match.params.productId;
         const response = await ApiFactory.getInstance().get(`/api/products/${id}`);
@@ -22,33 +22,33 @@ export default class Product extends Component {
             description: response.data.description,
             price: response.data.price,
             images: response.data.images,
-        });        
+        });
         console.log(response, 'cia data');
-        
+
     }
 
-    handleValueChange = ({target}) => {
+    handleValueChange = ({ target }) => {
         // Pakeicia atitinkama prop.
-        this.setState({[target.name]: target.value});
+        this.setState({ [target.name]: target.value });
     }
-    
+
     addNewImageUrl() {
-        if(this.state.newImage != '') {
+        if (this.state.newImage != '') {
             let temp = this.state.images;
-            temp.push({id: idGenerator, url:this.state.newImage} );
-            this.setState({ images: temp}); 
+            temp.push({ id: idGenerator, url: this.state.newImage });
+            this.setState({ images: temp });
             idGenerator++;
-            this.setState({newImage:''});
+            this.setState({ newImage: '' });
             alert("Image added successfully!");
         }
     }
 
     removeImage(id) {
-        let index = this.state.images.findIndex(x=>x.id == id);
-        if(index>=0){
+        let index = this.state.images.findIndex(x => x.id == id);
+        if (index >= 0) {
             let temp = this.state.images;
-            temp = temp.filter(x=>x.id!=id);   
-            this.setState({images:temp}); 
+            temp = temp.filter(x => x.id != id);
+            this.setState({ images: temp });
         }
     }
 
@@ -64,62 +64,83 @@ export default class Product extends Component {
     }
 
     render() {
-    return (
-      <>
-        <div>
-            <label><h1>Product</h1></label>
-            <div>
-                <label><h2>Name:</h2></label>
-                <input
-                    type="text" 
-                    name="name"
-                    value={this.state.name}
-                    onChange={this.handleValueChange}/>
-                <br/>
-            </div>
+        return (
+            <>
+                <h4 className="top-title"> Add new Product</h4>
+                <div className="container">
+                    <form>
+                        <ul className="flex-outer">
+                            <li>
+                                <label>Name:</label>
+                                <input
+                                    type="text"
+                                    name="name"
+                                    value={this.state.name}
+                                    onChange={this.handleValueChange} />
 
-            <div>
-                <label><h2>Price:</h2></label>
-                <input
-                    type="number" 
-                    name="price"
-                    value={this.state.price}
-                    onChange={this.handleValueChange}/>             
-                <br/>
 
-            </div>
 
-            <div>
-                <label><h2>Description:</h2></label>
-                <textarea
-                    type="text" 
-                    name="description"
-                    value={this.state.description}
-                    onChange={this.handleValueChange}/>
-                {/* <h1>{this.state.description}</h1> */}
-            </div>
 
-            <div>
-                <label><h2>Image Urls:</h2></label>
-                {this.state.images.map(image => {
-                    return <div>
-                        <img src={image.url} alt=""/>
-                        <button onClick={()=>this.removeImage(image.id)}>Remove</button>
-                    </div>
-                })
-                }
-               <label htmlFor=""><h2>Add new Url</h2></label>
-               <textarea
-                    name="newImage"
-                    onChange={this.handleValueChange} 
-                    value={this.state.newImage}
-                    type="text" /> 
-               <button onClick={() =>this.addNewImageUrl()}>Add</button>
-            </div>
-            <button onClick={()=> this.updateItem()}>Save changes</button>
-            <button><Link to="/admin">Return back</Link></button>
-        </div>
-      </>
-    )
-  }
+                            </li>
+                            <li>
+                                <label>Price:</label>
+                                <input
+                                    type="number"
+                                    name="price"
+                                    value={this.state.price}
+                                    onChange={this.handleValueChange} />
+
+
+                            </li>
+                            <li>
+
+                                <label>Description:</label>
+                                <textarea
+                                    rows="10"
+                                    type="text"
+                                    name="description"
+                                    value={this.state.description}
+                                    onChange={this.handleValueChange} />
+                                {/* <h1>{this.state.description}</h1> */}
+
+                            </li>
+                            <li>
+
+                                <label>Image Urls:</label>
+                                {this.state.images.map(image => {
+                                    return <div className="images">
+                                        <img src={image.url} alt="" />
+                                        <button onClick={() => this.removeImage(image.id)}>X</button>
+                                    </div>
+                                })
+                                }
+                            </li>
+
+                            <li>
+                                <label htmlFor="">Add new Url</label>
+                                <textarea
+                                    name="newImage"
+                                    onChange={this.handleValueChange}
+                                    value={this.state.newImage}
+                                    type="text" />
+                            </li>
+
+                            <li>
+                                <button onClick={() => this.addNewImageUrl()}>Add</button>
+                            </li>
+
+                            <li>
+                                <button onClick={() => this.updateItem()}>Save changes</button>
+                            </li>
+
+                            <li>
+                                <button><Link to="/admin">Return back</Link></button>
+                            </li>
+                        </ul>
+
+                    </form>
+                </div>
+            </>
+        )
+    }
 }
